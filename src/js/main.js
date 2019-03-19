ec29811969ba8837ed621bbe636b8052b2aa8563
Anar <a.agaev@smileexpo.ru>
2019-03-19 12:28:43 +0300

    Added About company card and added Hide Material slider blocks when user click ourside container

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,27 @@ $( document ).ready(function() {
 
         $('.material-slider').addClass('visible')
         $('#' + cardId).addClass('active')
+        $('body').addClass('modal-open')
+    })
+
+    // Show material card About Company when user clickd About company link in main navigation
+    $('a[data-card-id="about-company"]').on('click', function(event){
+        event.preventDefault()
+        let cardId = $(this).data('card-id')
+
+        $('.material-slider').addClass('visible')
+        $('#' + cardId).addClass('active')
+        $('body').addClass('modal-open')
+    })
+
+    // Smooth scroll to the target on page when user click anchor in the main navegation
+    $('.nav__item [data-anchor-target]').on('click', function functionName(event) {
+        event.preventDefault()
+        let anchorTargetId = $(this).data('anchor-target')
+
+        if ( $(anchorTargetId).length != 0 ) {
+    	    $('html, body').animate({ scrollTop: $(anchorTargetId).offset().top }, 500);
+        }
     })
 
     // Hide material card when user clickd button close
@@ -20,20 +41,30 @@ $( document ).ready(function() {
         let  tooSide = $(this).attr('data-to-side')
         slideMaterialCard(tooSide)
     })
+
+    // HIDE MATERIAL CARD WHEN USER CLICKD OUTSIDE BLOCK
+	$(document).on('mouseup', function(event) {
+		let targetBlock = $('.material-slider__container')
+
+        if (!targetBlock.is(event.target)
+            && targetBlock.has(event.target).length === 0) {
+                hideMaterialCard()
+        }
+	})
 })
 
 // FUNCTION FOR HIDE MATERIAL CARD
 function hideMaterialCard() {
     $('.material-slider').removeClass('visible')
     $('.material-slider__container').css('marginTop','0').removeClass('active')
+    $('body').removeClass('modal-open')
 }
-
 
 // FUNCTION FOR SLIDEING MATERIAL CARDS INSIDE CARD SLIDER
 function slideMaterialCard(tooSide) {
     let activeCard = $('.material-slider__container.active').index()
-    let cards = $('.material-slider__container')
-    let numCards = $('.material-slider__container').length
+    let cards = $('.material-slider__container:not(.single-card)')
+    let numCards = $('.material-slider__container:not(.single-card)').length
 
     // Changed number of active card
     switch (tooSide) {
