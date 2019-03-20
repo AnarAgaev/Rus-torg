a324ee78b583766a22fe45ff0ca3174059b4f26d
Anar <a.agaev@smileexpo.ru>
2019-03-20 09:37:34 +0300

    Fix animation in material slider

--- src/js/main.js
+++ src/js/main.js
@@ -82,3 +82,74 @@ function slideMaterialCard(tooSide) {
     $('.material-slider__container.active').css('marginTop','-100%').removeClass('active')
     cards.eq(activeCard).css('marginTop','0').addClass('active')
 }
+
+// WIDTH ONE SLIDE PLUS RIGHT INDENT
+const STEP_WIDTH = 410
+
+// FUNCTION FOR INITIALIZATION MATERIAL SLIDER
+function materialSliderInit() {
+    let sliderContainer = $('.material__items')
+    let firstElement = $('.material__item:first')
+    let lastElement = $('.material__item:last')
+    let positionLeft = sliderContainer.position().left
+
+    // Cloning first and last element
+    firstElement.clone().appendTo(sliderContainer)
+    lastElement.clone().prependTo(sliderContainer)
+
+    // Moove to the left slider container on the step width
+    positionLeft -= STEP_WIDTH
+    sliderContainer.css('left', positionLeft + 'px')
+}
+// INITIALIZATION MATERIAL SLIDER
+materialSliderInit()
+
+// FUNCTION FOR SLIDING MATERIAL SLIDER
+function materialSliding(slideTo) {
+    let sliderContainer = $('.material__items')
+    let positionLeft = sliderContainer.position().left
+    let firstElement = $('.material__item:first')
+    let lastElement = $('.material__item:last')
+
+    switch (slideTo) {
+        case 'left':
+            positionLeft += STEP_WIDTH
+            sliderContainer.animate(
+                {left: positionLeft + 'px'},
+                300,
+                function(){
+                    let activeElement = $('.material__item_active')
+                    activeElement.addClass('material__item_passive').removeClass('material__item_active')
+                    $('.material__item:last').prependTo(sliderContainer) // Cloning last element to the front
+                    lastElement.remove() // Deleted last element
+                    let numElements = $('.material__item').length - 2 // Number of the last but one element
+                    $('.material__item:eq(' + numElements + ')').clone().prependTo(sliderContainer) // Cloning last but one element to the front
+                    sliderContainer.css('left', '-' + STEP_WIDTH + 'px') // Moove slider container to the start position
+                    $('.material__item:eq(2)').removeClass('material__item_passive').addClass('material__item_active')
+                })
+            break
+        case 'right':
+            positionLeft -= STEP_WIDTH
+            sliderContainer.animate(
+                {left: positionLeft + 'px'},
+                300,
+                function(){
+                    let activeElement = $('.material__item_active')
+                    activeElement.addClass('material__item_passive').removeClass('material__item_active')
+                    firstElement.remove() // Deleted first element
+                    $('.material__item:eq(1)').clone().appendTo(sliderContainer) // Cloning last but one element to the front
+                    sliderContainer.css('left', '-' + STEP_WIDTH + 'px')
+                    $('.material__item').removeClass('material__item_active')
+                    $('.material__item:eq(2)').removeClass('material__item_passive').addClass('material__item_active')
+                })
+            break
+    }
+}
+
+// HANDLE CLICK ON TO THE LEFT OR TO THE RIGHT IN MATERIAL SLIDER
+$('.material__arr_left').on('click', function(){
+    materialSliding('left')
+})
+$('.material__arr_right').on('click', function(){
+    materialSliding('right')
+})
